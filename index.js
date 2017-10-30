6437774c27a7f43e4a690f17e584fe385a7e3be2
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-10-30 19:44:14 +0530

    New Test Performed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("America/Los_Angeles");
     console.log("Test Build");
+    console.log("Another Test");
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
