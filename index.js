06d1fc5fba4732531f7ad91fc366c517eafc8dce
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-10-31 06:30:10 +0530

    New Test Performed-q

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("Asia/Calcutta");
-    console.log("Old Test yes.");
+    console.log("Old Test - Yes.");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Trivandrum is: ' + currentTime.toString(),
     });
 };
