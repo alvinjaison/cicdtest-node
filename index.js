c1056fe0eb6dde3b1078c1b75620ce472b1e435f
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-10-31 05:38:23 +0530

    New Test Performed-tw

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Asia/Calcutta");
     console.log("Old Test yes.");
     callback(null, {
         statusCode: '200',
