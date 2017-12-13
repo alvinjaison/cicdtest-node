15f0926b9fe3291d4e04dad6fcd133b969f83f2c
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-12-13 14:46:24 +0530

    Updated a Bug

--- delandstartapi.sh
+++ delandstartapi.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #### Script to delete CICDApi
 
-export AWS_ACCESS_KEY_ID=AKIAJRNLY7NC437WMGOA
-export AWS_SECRET_ACCESS_KEY=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
-
-apigwid=($(aws --profile gunbro apigateway get-rest-apis | jq '.items[] | select(.name=="CICDApi") | .id' | sed 's/"//g'))
+apigwid=($(aws --profile cct apigateway get-rest-apis | jq '.items[] | select(.name=="CICDNodeApi") | .id' | sed 's/"//g'))
 if [ $? -ne 0 ]; then 
-    aws --profile gunbro apigateway delete-rest-api --rest-api-id ${apigwid} --region us-east-2
+    aws --profile cct apigateway delete-rest-api --rest-api-id ${apigwid} --region us-east-2
 fi
 
-aws --profile gunbro apigateway delete-base-path-mapping --domain-name 'cicdtest-staging.appcohesion.io' --base-path 'CICDApi'
-aws --profile gunbro lambda delete-function --function-name CICDFunction --region us-east-2
-aws --profile gunbro codepipeline start-pipeline-execution --name AppCoApiPL --region us-east-2
+aws --profile cct apigateway delete-base-path-mapping --domain-name 'cicdtest-staging.appcohesion.io' --base-path 'CICDNodeApi'
+aws --profile cct lambda delete-function --function-name cicdTestNode --region us-east-2
+aws --profile cct codepipeline start-pipeline-execution --name CCTTest-Node --region us-east-2
