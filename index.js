56b74d8810a2775de8adc785244973fd385efd32
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-12-19 18:32:56 +0530

    Updated with comments in YAML files

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+/**
+ * @version: 0.8
+ * @author: Flicked from online.
+ * @desc: A sample Node based Lambda to print the Time of a particaular timezone.
+ */
+
 var time = require('time');
 
+// Lambda handler.
 exports.handler = (event, context, callback) => {
+
     var currentTime = new time.Date(); 
     currentTime.setTimezone("Asia/Calcutta");
-    console.log("Old Test - Yes.");
+
+    // I am done.
     callback(null, {
         statusCode: '200',
         body: 'The time in Trivandrum is: ' + currentTime.toString(),
     });
+
 };
